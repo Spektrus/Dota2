da43fd3187249fc7a9decb441a23f9a37ee1c8f6
Spektrus <Spektrus@users.noreply.github.com>
2017-10-31 16:28:47 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
 	session_start();
 	$siteTitle = "Projekt 2";
-	$db = new mysqli('beckius.me', 'mabe1317', 'cDBtWSX9sCsjBmNc', 'projekt') or die("Fel vid anslutning"); // Databasanslutning
-	$apikey = "EC70849F4F195F151784D729025C9960"; // API-key
+	$db = new mysqli('secret') or die("Fel vid anslutning"); // Databasanslutning
+	$apikey = "secret"; // API-key
 	error_reporting(1);
 	ini_set('display_errors', 1);
 ?>
